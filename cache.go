9f17c3f594103fc1f71492bf1dfe223d86455084
Alex Chan <alyxchan87@gmail.com>
2024-10-15 16:15:36 -0700

    Finish cache implementation

--- cache.go
+++ cache.go
@@ -8,6 +8,8 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+var defaultExpiration = time.Hour * 24
+
 func connectToRedis(redisConfig *Config) (*redis.Client, error) {
 	if redisConfig == nil {
 		return nil, fmt.Errorf("no Redis config provided")
@@ -28,10 +30,38 @@ func connectToRedis(redisConfig *Config) (*redis.Client, error) {
 	return client, nil // Return the connected Redis client
 }
 
-func getCachedReponse(url string) ([]byte, error) {
-	return nil, nil
+func getCachedReponse(redisClient *redis.Client, url string) ([]byte, error) {
+	if redisClient == nil {
+		return nil, fmt.Errorf("cannot get cached response: no Redis client provided")
+	}
+
+	if url == "" {
+		return nil, fmt.Errorf("url cannot be empty")
+	}
+
+	val, err := redisClient.Get(context.Background(), url).Result()
+	// TODO: again does this return nil or redis.nil on success?
+	if err != redis.Nil {
+		return nil, fmt.Errorf("failed to get cached response: %w", err)
+	}
+
+	return []byte(val), nil
 }
 
-func setCachedResponse(url string, response []byte, expiration time.Duration) error {
-	return nil
+func setCachedResponse(redisClient *redis.Client, url string, response []byte, expiration time.Duration) error {
+	if redisClient == nil {
+		return fmt.Errorf("cannot set cached response: no Redis client provided")
+	}
+	if url == "" || response == nil {
+		return fmt.Errorf("url and/or response cannot be empty")
+	}
+
+	if expiration == 0 {
+		expiration = defaultExpiration
+	}
+
+	err := redisClient.Set(context.Background(), url, response, expiration)
+
+	// TODO: does this return nil or redis.nil on success?
+	return err.Err()
 }
